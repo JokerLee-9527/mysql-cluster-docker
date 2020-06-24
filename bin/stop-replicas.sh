e28acfad60be0df12f9a6147e172ed1563396686
JokerLee <lijian79@gmail.com>
2020-06-24 17:26:51 +0800

    br_windows

--- bin/stop-replicas.sh
+++ bin/stop-replicas.sh
@@ -1,5 +1,9 @@
 #/bin/bash
+# set -x 
+
 #停止集群复制
 passwd=$(sed '/SLAVE_PASSWD/!d;s/.*=//' ../.env)
-docker exec -it  mysql-slave mysql -uroot -p${passwd} \
+winpty docker.exe exec -it  mysql-slave mysql -uroot -p${passwd} \
 -e "STOP SLAVE"
+
+#set +x
