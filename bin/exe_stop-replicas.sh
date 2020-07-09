66265daa6b7f28798dc57bd577dad7d95208f2ca
JokerLee <lijian79@gmail.com>
2020-07-09 16:18:06 +0800

    添加一下脚本

--- bin/exe_stop-replicas.sh
+++ bin/exe_stop-replicas.sh
@@ -3,7 +3,7 @@
 
 #停止集群复制
 passwd=$(sed '/SLAVE_PASSWD/!d;s/.*=//' ../.env)
-docker exec -it  mysql-slave mysql -uroot -p${passwd} \
+docker.exe exec -it  mysql-slave mysql -uroot -p${passwd} \
 -e "STOP SLAVE"
 
 #set +x
